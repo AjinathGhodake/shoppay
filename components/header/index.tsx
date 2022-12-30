689c56e6c6bbeda2acc5cbc04ee3ecdff360111d
Ajinath ghodake <ajinath.ghodake@coditas.com>
2022-12-30 12:16:46 +0530

    created new components header added banner image

---
+++ components/header/index.tsx
@@ -0,0 +1,5 @@
+import Ad from './Ad';
+import styles from './styles.module.scss';
+export default function Header() {
+  return <header className={styles.header}><Ad /></header>;
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,21 @@ import { Provider } from 'react-redux';
 import store from '../store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist';
-
+import Head from 'next/head';
 
 let persistor = persistStore(store);
 export default function App({ Component, pageProps }: AppProps) {
-  return (<Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <Component {...pageProps} />
-    </PersistGate>
+  return (<>
+    <Head>
+      <title>Shoppay</title>
+      <meta name="description" content="Shoppay - online shopping what do you needs." />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
 
-  </Provider>);
+    </Provider></>);
 }
